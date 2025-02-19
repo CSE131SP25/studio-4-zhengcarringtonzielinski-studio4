9c87b2bb3e2291fcaa9f90a1a4a605d954c5b400
Ednaz222 <zhenge2@bxscience.edu>
2025-02-19 14:10:58 -0600

    Finished Studio 04

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,55 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+	
+		if (shape.equals("rectangle")) {
+			double parameterOne = in.nextDouble();
+			double parameterTwo = in.nextDouble();
+			double parameterThree = in.nextDouble();
+			double parameterFour = in.nextDouble();
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			if (isFilled == true) {
+				StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			else {
+				StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			
+			
+		}
+		else if (shape.equals("ellipse")) {
+			double parameterOne = in.nextDouble();
+			double parameterTwo = in.nextDouble();
+			double parameterThree = in.nextDouble();
+			double parameterFour = in.nextDouble();
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			if (isFilled == true) {
+				StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			else {
+				StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
+		else if (shape.equals("triangle")) {
+			double[] xCor = new double [3];
+			double[] yCor = new double [3];
+			for (int i = 0; i < 3; i++) {
+				xCor[i] = in.nextDouble();
+				yCor[i] = in.nextDouble();
+			}
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			if (isFilled == true) {
+				StdDraw.filledPolygon( xCor, yCor);
+			}
+			else {
+				StdDraw.polygon(xCor, yCor);
+			}
+		}
 		
 	}
 }
